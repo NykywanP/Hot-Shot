5580286e1cf7d72a816f6ca3a65fbb19226e2840
nykywan.parker <nykywan.parker@nykywanparkers-Mac-mini.local>
2020-11-09 10:02:28 -0500

    [app index style] add file to object

--- app.js
+++ app.js
@@ -1,43 +1,71 @@
-function draw() {
-  var drawing = document.getElementById("drawing");
-  var con = drawing.getContext("2d");
-  var picCan = document.getElementById("myCanvas");
-  con.drawImage(picCan, 0, 0, 40, 40);
-  var image2 = new Image();
-  image2.src = "basketB";
-  con.drawImage(image2, 20, 20, 40, 30);
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var raf;
+var running = false;
+
+var ball = {
+  x: 100,
+  y: 100,
+  vx: 5,
+  vy: 1,
+  radius: 40,
+  color: 'red',
+  draw: function() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+};
+
+function clear() {
+  ctx.fillStyle = 'rgba(400, 400, 400, 0.3)';
+  ctx.fillRect(0,0,canvas.width,canvas.height);
 }
-console.log(draw)
 
-canvasNode.width  = 100; // in pixels
-canvasNode.height = 80; // in pixels
-// Bring the picture into the canvas
+function draw() {
+  clear();
+  ball.draw();
+  ball.x += ball.vx;
+  ball.y += ball.vy;
 
-//function myCa() { 
-  ///let myCa = canvas.getContext("2d");
- //var canvas = document.getElementById("myCanvas");
-  //var ctx = canvas.getContext("2d");
-  //var x = canvas.width/2;
- //var y = canvas.height-30;
-  //var dx = 2;
- //var dy = -2;
- // }
- // function drawBall() {
-  //    ctx.beginPath();
- //     ctx.arc(x, y, 10, 0, Math.PI*2);
-  //    ctx.fillStyle = "#0095DD";
-  //    ctx.fill();
-  //    ctx.closePath();
- // }
-  //console.log(drawBall)
- 
-  //const ctx = draw;
-  //function drawTwo() {
-   //   ctx.clearRect(0, 0, canvas.width, canvas.height);
-   //   drawBall();
-    //  x += dx;
-     // y += dy;
-  //}
-  
-  ///setInterval(draw, 10);
+  if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0) {
+    ball.vy = -ball.vy;
+  }
+  if (ball.x + ball.vx > canvas.width || ball.x + ball.vx < 0) {
+    ball.vx = -ball.vx;
+  }
 
+  raf = window.requestAnimationFrame(draw);
+}
+
+canvas.addEventListener('mousemove', function(e) {
+  if (!running) {
+    clear();
+    ball.x = e.clientX;
+    ball.y = e.clientY;
+    ball.draw();
+  }
+});
+
+canvas.addEventListener('click', function(e) {
+  if (!running) {
+    raf = window.requestAnimationFrame(draw);
+    running = true;
+  }
+});
+
+canvas.addEventListener('mouseout', function(e) {
+  window.cancelAnimationFrame(raf);
+  running = false;
+});
+
+  let cx = document.querySelector("canvas").getContext("2d");
+  let img = document.createElement("img");
+  img.src = "https://banner2.cleanpng.com/20180320/qyw/kisspng-backboard-ncaa-men-s-division-i-basketball-tournam-basketball-hoop-png-transparent-5ab1b00c2d0b99.9652717415215943801845.jpg";
+  img.addEventListener("load", () => {
+    for (let x = 10; x < 200; x += 30) {
+      cx.drawImage(img, x, 10);
+    }
+  });
