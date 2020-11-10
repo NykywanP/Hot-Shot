db21db9a36c46afeb9755a390d6259a79821adcd
nykywan.parker <nykywan.parker@nykywankersMini.lan>
2020-11-10 01:04:38 -0500

    [DS_Store Lit-Troy-I-wanna-be-a baller.mp3 app.js index.html style.css] add object to file

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var raf;
 var running = false;
 
 var ball = {
-  x: 100,
-  y: 100,
+  x: 10,
+  y: 10,
   vx: 5,
   vy: 1,
   radius: 40,
-  color: 'red',
+  color: '#C9082A',
   draw: function() {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
@@ -29,7 +29,6 @@ function draw() {
   ball.draw();
   ball.x += ball.vx;
   ball.y += ball.vy;
-
   if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0) {
     ball.vy = -ball.vy;
   }
@@ -46,7 +45,9 @@ canvas.addEventListener('mousemove', function(e) {
     ball.x = e.clientX;
     ball.y = e.clientY;
     ball.draw();
+    drawGoal();
   }
+  console.log(ball)
 });
 
 canvas.addEventListener('click', function(e) {
@@ -61,11 +62,78 @@ canvas.addEventListener('mouseout', function(e) {
   running = false;
 });
 
-  let cx = document.querySelector("canvas").getContext("2d");
-  let img = document.createElement("img");
-  img.src = "https://banner2.cleanpng.com/20180320/qyw/kisspng-backboard-ncaa-men-s-division-i-basketball-tournam-basketball-hoop-png-transparent-5ab1b00c2d0b99.9652717415215943801845.jpg";
-  img.addEventListener("load", () => {
-    for (let x = 10; x < 200; x += 30) {
-      cx.drawImage(img, x, 10);
-    }
+  // let cx = document.querySelector("canvas").getContext("2d");
+  // let img = document.createElement("img");
+  // img.src = "https://banner2.cleanpng.com/20180320/qyw/kisspng-backboard-ncaa-men-s-division-i-basketball-tournam-basketball-hoop-png-transparent-5ab1b00c2d0b99.9652717415215943801845.jpg";
+  // img.addEventListener("load", () => {
+  //   for (let x = 10; x < 200; x += 30) {
+  //     cx.drawImage(img, x, 10);
+  //   }
+  // });
+
+  function drawGoal() {
+     let cx = document.querySelector("canvas").getContext("2d");
+     let img = document.createElement("img");
+     img.src = "https://banner2.cleanpng.com/20180320/qyw/kisspng-backboard-ncaa-men-s-division-i-basketball-tournam-basketball-hoop-png-transparent-5ab1b00c2d0b99.9652717415215943801845.jpg";
+     img.addEventListener("load", () => {
+       for (let x = 10; x < 200; x += 30) {
+         cx.drawImage(img, x, 10);
+       }
   });
+   }
+  
+
+   var timer;
+var minute = 2
+var seconds = minute * 60
+var textSec = "00"
+var display = document.getElementById("display");
+display.innerHTML = minute + ':00'
+
+function start(){
+    var statSec = 60
+    timer = setInterval(function(){
+        seconds--;
+        if(statSec != 0)
+            statSec--
+        else
+            statSec = 59
+
+        if(statSec < 10)
+            textSec = '0' + statSec
+        else 
+            textSec = statSec
+
+        display.innerHTML = Math.floor(seconds/60) + ':' + textSec
+
+        if(seconds == 0)
+            clearInterval(timer)
+    }, 1000)
+}
+
+function stop(){
+    clearInterval(timer)
+}
+
+function cancel(){
+    console.log("asdasd");
+    clearInterval(timer)
+    display.innerHTML = minute + ':00'
+    seconds = minute * 60
+}
+var resetButton = document.getElementById('resetButton');
+console.log(resetButton)
+resetButton.onclick= reloadPage;
+
+function reloadPage(){
+   window.location.reload();
+};
+
+
+
+function scoreCount(){
+  var score = document.getElementById('score')
+  // score++;
+console.log(score)
+}
+scoreCount()
